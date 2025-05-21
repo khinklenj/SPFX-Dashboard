6e77f6401d8e9468560523e3162d2e4c7c566907
Keith Hinkle <khinklenj@gmail.com>
2025-05-21 12:28:42 -0400

    Base

---
+++ src/webparts/dashboard/components/MyDay/MyDay.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Text } from '@fluentui/react';
+
+export const MyDay: React.FC = () => {
+    return <Text variant="medium">[MyDay] component placeholder</Text>;
+};
---
+++ src/webparts/dashboard/components/MyFiles/MyFiles.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Text } from '@fluentui/react';
+
+export const MyFiles: React.FC = () => {
+    return <Text variant="medium">[MyFiles] component placeholder</Text>;
+};
